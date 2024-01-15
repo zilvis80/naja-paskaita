3c1325c4b49ae0c259010c351e02d85530770677
zilvinas <zilvis80@mail.ru>
2024-01-15 22:56:45 +0200

    for

---
+++ forof.js
@@ -0,0 +1,47 @@
+// let zodziai = ['tvora', 'vartas', 'durys', 'automobilis', 'dviratis'];
+// let indeksas = 0;
+
+// for (let daiktai of zodziai) {
+//   console.log('indeksas', indeksas, 'zodis', daiktai);
+//   indeksas++;
+// }
+
+// let pirkiniai = ['pienas', 'kiausiniai', 'mesa', 'duona', 'obuoliai'];
+// // console.log('pirkiniu krepselis:', pirkiniai.length);
+// for (let i = 0; i < pirkiniai.length; i++) {
+//   console.log('---', pirkiniai(i));
+// }
+// let pazymiai = [5, 6, 7, 4, 9];
+// let vidurkis;
+// let suma = 0;
+// for (let pazymis of pazymiai) {
+//   suma = suma + pazymis;
+
+//   console.log('pazymis=', pazymis);
+//   vidurkis = suma / pazymiai.length;
+//   console.log('vidurkis:', vidurkis);
+// }
+// let kilometrai = [120, 150, 90, 45, 70];
+
+// for (let km of kilometrai) {
+//   console.log('kilometrai:', km);
+
+//   if (km >= 85) {
+//     console.log('didesnis uz 85=', km);
+//   }
+// }
+// let auto = ['audi', 'ford', 'bmw'];
+// for (let am of auto) {
+//   console.log(am, 'raidziu skaicius', auto.length);
+// }
+// let klaida = ['k12', 'b18', 'c13', 'g22'];
+// for (let kl of klaida) {
+//   console.log('klaida:', kl, '-', 'atminties nera');
+// }
+let pirmas_studentas = [5, 6, 4, 3, 9];
+let antras_studentas = [8, 3, 4, 6, 7];
+console.log('pimas mokinio pazymiai:');
+for (let paz of pirmas_studentas);
+{
+  console.log(paz);
+}
